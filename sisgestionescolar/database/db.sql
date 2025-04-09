4dcc8c2c1fac78dd749f057541cd0ec6e1e82bbc
santiagoMP28 <cocandersonclanero2016@gmail.com>
2025-04-09 07:45:16 -0500

    Actualizar estructura de base de datos para PostgreSQL

--- sisgestionescolar/database/db.sql
+++ sisgestionescolar/database/db.sql
@@ -1,211 +1,231 @@
--- Base de datos: sisgestionescolar
 
--- --------------------------------------------------------
+CREATE TABLE roles (
 
--- Estructura de tabla para la tabla administrativos
+  id_rol        INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nombre_rol    VARCHAR (255) NOT NULL UNIQUE KEY,
 
-CREATE TABLE administrativos (
-  id_administrativo SERIAL PRIMARY KEY,
-  persona_id INT NOT NULL,
-  fyh_creacion TIMESTAMP DEFAULT NULL,
-  fyh_actualizacion TIMESTAMP DEFAULT NULL,
-  estado VARCHAR(11) DEFAULT NULL
-);
+  fyh_creacion   DATETIME NULL,
+  fyh_actualizacion DATETIME NULL,
+  estado        VARCHAR (11)
 
--- --------------------------------------------------------
+)ENGINE=InnoDB;
+INSERT INTO roles (nombre_rol,fyh_creacion,estado) VALUES  ('ADMINISTRADOR','2024-01-03 16:20:20','1');
+INSERT INTO roles (nombre_rol,fyh_creacion,estado) VALUES  ('DIRECTOR ACADÉMICO','2024-01-03 16:20:20','1');
+INSERT INTO roles (nombre_rol,fyh_creacion,estado) VALUES  ('DIRECTOR ADMINISTRATIVO','2024-01-03 16:20:20','1');
+INSERT INTO roles (nombre_rol,fyh_creacion,estado) VALUES  ('CONTADOR','2024-01-03 16:20:20','1');
+INSERT INTO roles (nombre_rol,fyh_creacion,estado) VALUES  ('SECRETARIA','2024-01-03 16:20:20','1');
+INSERT INTO roles (nombre_rol,fyh_creacion,estado) VALUES  ('DOCENTE','2024-01-03 16:20:20','1');
 
--- Estructura de tabla para la tabla asignaciones
+CREATE TABLE usuarios (
 
-CREATE TABLE asignaciones (
-  id_asignacion SERIAL PRIMARY KEY,
-  docente_id INT NOT NULL,
-  nivel_id INT NOT NULL,
-  grado_id INT NOT NULL,
-  materia_id INT NOT NULL,
-  fyh_creacion TIMESTAMP DEFAULT NULL,
-  fyh_actualizacion TIMESTAMP DEFAULT NULL,
-  estado VARCHAR(11) DEFAULT NULL
-);
+  id_usuario    INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  rol_id        INT (11) NOT NULL,
+  email         VARCHAR (255) NOT NULL UNIQUE KEY,
+  password      TEXT NOT NULL,
 
--- --------------------------------------------------------
+  fyh_creacion   DATETIME NULL,
+  fyh_actualizacion DATETIME NULL,
+  estado        VARCHAR (11),
 
--- Estructura de tabla para la tabla configuracion_instituciones
+  FOREIGN KEY (rol_id) REFERENCES roles (id_rol) on delete no action on update cascade
 
-CREATE TABLE configuracion_instituciones (
-  id_config_institucion SERIAL PRIMARY KEY,
-  nombre_institucion VARCHAR(255) NOT NULL,
-  logo VARCHAR(255) DEFAULT NULL,
-  direccion VARCHAR(255) NOT NULL,
-  telefono VARCHAR(100) DEFAULT NULL,
-  celular VARCHAR(100) DEFAULT NULL,
-  correo VARCHAR(100) DEFAULT NULL,
-  fyh_creacion TIMESTAMP DEFAULT NULL,
-  fyh_actualizacion TIMESTAMP DEFAULT NULL,
-  estado VARCHAR(11) DEFAULT NULL
-);
+)ENGINE=InnoDB;
+INSERT INTO usuarios (rol_id,email,password,fyh_creacion,estado)
+VALUES ('1','admin@admin.com','$2y$10$0tYmdHU9uGCIxY1f90W1EuIm54NQ8axowkxL1WzLbqO2LdNa8m3l2','2023-12-28 20:29:10','1');
 
--- Volcado de datos para la tabla configuracion_instituciones
-
-INSERT INTO configuracion_instituciones (id_config_institucion, nombre_institucion, logo, direccion, telefono, celular, correo, fyh_creacion, fyh_actualizacion, estado) VALUES
-(1, 'Hilari Web School', 'logo.jpg', 'Zona Los Olivos Calle Max Toledo Av. 6 nro 100', '2228837', '59175657007', 'info@hilariweb.com', '2023-12-28 20:29:10', NULL, '1'),
-(2, 'udec', '2025-04-04-08-42-43escudo_udec.png', 'los olivos', '78864748', '65765859', 'udec@udec.com', '2025-04-04 08:42:43', NULL, '1');
-
--- --------------------------------------------------------
-
--- Estructura de tabla para la tabla docentes
-
-CREATE TABLE docentes (
-  id_docente SERIAL PRIMARY KEY,
-  persona_id INT NOT NULL,
-  especialidad VARCHAR(255) NOT NULL,
-  antiguedad VARCHAR(255) NOT NULL,
-  fyh_creacion TIMESTAMP DEFAULT NULL,
-  fyh_actualizacion TIMESTAMP DEFAULT NULL,
-  estado VARCHAR(11) DEFAULT NULL
-);
-
--- Volcado de datos para la tabla docentes
-
-INSERT INTO docentes (id_docente, persona_id, especialidad, antiguedad, fyh_creacion, fyh_actualizacion, estado) VALUES
-(1, 9, 'robar', '22', '2025-04-01 14:26:43', '2025-04-01 18:43:03', '1');
-
--- --------------------------------------------------------
-
--- Estructura de tabla para la tabla estudiantes
-
-CREATE TABLE estudiantes (
-  id_estudiante SERIAL PRIMARY KEY,
-  persona_id INT NOT NULL,
-  nivel_id INT NOT NULL,
-  grado_id INT NOT NULL,
-  rude VARCHAR(50) NOT NULL,
-  fyh_creacion TIMESTAMP DEFAULT NULL,
-  fyh_actualizacion TIMESTAMP DEFAULT NULL,
-  estado VARCHAR(11) DEFAULT NULL
-);
-
--- --------------------------------------------------------
-
--- Estructura de tabla para la tabla gestiones
-
-CREATE TABLE gestiones (
-  id_gestion SERIAL PRIMARY KEY,
-  gestion VARCHAR(255) NOT NULL,
-  fyh_creacion TIMESTAMP DEFAULT NULL,
-  fyh_actualizacion TIMESTAMP DEFAULT NULL,
-  estado VARCHAR(11) DEFAULT NULL
-);
-
--- Volcado de datos para la tabla gestiones
-
-INSERT INTO gestiones (id_gestion, gestion, fyh_creacion, fyh_actualizacion, estado) VALUES
-(1, 'GESTIÓN 2024', '2023-12-28 20:29:10', NULL, '1'),
-(2, 'gestuon 2025', '2025-04-04 08:43:06', NULL, '1');
-
--- --------------------------------------------------------
-
--- Estructura de tabla para la tabla grados
-
-CREATE TABLE grados (
-  id_grado SERIAL PRIMARY KEY,
-  nivel_id INT NOT NULL,
-  curso VARCHAR(255) NOT NULL,
-  paralelo VARCHAR(255) NOT NULL,
-  fyh_creacion TIMESTAMP DEFAULT NULL,
-  fyh_actualizacion TIMESTAMP DEFAULT NULL,
-  estado VARCHAR(11) DEFAULT NULL
-);
-
--- Volcado de datos para la tabla grados
-
-INSERT INTO grados (id_grado, nivel_id, curso, paralelo, fyh_creacion, fyh_actualizacion, estado) VALUES
-(1, 1, 'INICIAL - 1', 'A', '2023-12-28 20:29:10', NULL, '1'),
-(2, 1, 'INICIAL - 1', 'F', '2025-04-08 13:52:57', NULL, '1'),
-(3, 1, 'INICIAL - 1', 'D', '2025-04-08 13:53:03', '2025-04-08 13:53:10', '1');
-
--- --------------------------------------------------------
-
--- Estructura de tabla para la tabla materias
-
-CREATE TABLE materias (
-  id_materia SERIAL PRIMARY KEY,
-  nombre_materia VARCHAR(255) NOT NULL,
-  fyh_creacion TIMESTAMP DEFAULT NULL,
-  fyh_actualizacion TIMESTAMP DEFAULT NULL,
-  estado VARCHAR(11) DEFAULT NULL
-);
-
--- Volcado de datos para la tabla materias
-
-INSERT INTO materias (id_materia, nombre_materia, fyh_creacion, fyh_actualizacion, estado) VALUES
-(1, 'MATEMÁTICA', '2023-12-28 20:29:10', NULL, '1'),
-(2, 'FISICA', '2025-04-08 14:34:10', NULL, '1'),
-(3, 'ingles', '2025-04-08 14:34:17', '2025-04-08 14:34:27', '1');
-
--- --------------------------------------------------------
-
--- Estructura de tabla para la tabla niveles
-
-CREATE TABLE niveles (
-  id_nivel SERIAL PRIMARY KEY,
-  gestion_id INT NOT NULL,
-  nivel VARCHAR(255) NOT NULL,
-  turno VARCHAR(255) NOT NULL,
-  fyh_creacion TIMESTAMP DEFAULT NULL,
-  fyh_actualizacion TIMESTAMP DEFAULT NULL,
-  estado VARCHAR(11) DEFAULT NULL
-);
-
--- Volcado de datos para la tabla niveles
-
-INSERT INTO niveles (id_nivel, gestion_id, nivel, turno, fyh_creacion, fyh_actualizacion, estado) VALUES
-(1, 1, 'INICIAL', 'MAÑANA', '2023-12-28 20:29:10', NULL, '1'),
-(2, 1, 'PRIMARIA', 'MAÑANA', '2025-04-01 21:02:22', NULL, '1'),
-(3, 1, 'SECUNDARIA', 'MAÑANA', '2025-04-01 21:02:28', NULL, '1'),
-(4, 1, 'PRIMARIA', 'TARDE', '2025-04-04 08:43:57', NULL, '1');
-
--- --------------------------------------------------------
-
--- Estructura de tabla para la tabla personas
 
 CREATE TABLE personas (
-  id_persona SERIAL PRIMARY KEY,
-  usuario_id INT NOT NULL,
-  nombres VARCHAR(50) NOT NULL,
-  apellidos VARCHAR(50) NOT NULL,
-  ci VARCHAR(20) NOT NULL,
-  fecha_nacimiento VARCHAR(20) NOT NULL,
-  profesion VARCHAR(50) NOT NULL,
-  direccion VARCHAR(255) NOT NULL,
-  celular VARCHAR(20) NOT NULL,
-  fyh_creacion TIMESTAMP DEFAULT NULL,
-  fyh_actualizacion TIMESTAMP DEFAULT NULL,
-  estado VARCHAR(11) DEFAULT NULL
-);
 
--- Volcado de datos para la tabla personas
+  id_persona      INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  usuario_id             INT (11) NOT NULL,
+  nombres            VARCHAR (50) NOT NULL,
+  apellidos          VARCHAR (50) NOT NULL,
+  ci                 VARCHAR (20) NOT NULL,
+  fecha_nacimiento   VARCHAR (20) NOT NULL,
+  profesion          VARCHAR (50) NOT NULL,
+  direccion          VARCHAR (255) NOT NULL,
+  celular            VARCHAR (20) NOT NULL,
 
-INSERT INTO personas (id_persona, usuario_id, nombres, apellidos, ci, fecha_nacimiento, profesion, direccion, celular, fyh_creacion, fyh_actualizacion, estado) VALUES
-(1, 1, 'Freddy Eddy', 'Hilari Michua', '12345678', '10/10/1990', 'LICENCIADO EN EDUCACIÓN', 'Zona Los Pinos Av. Las Rosas Nro 100', '75657007', '2023-12-28 20:29:10', NULL, '1'),
-(9, 9, 'wasaaa', 'zazaz', '232312312312', '2025-04-01', 'ratero', 'los olivos', '12334566', '2025-04-01 14:26:43', '2025-04-01 18:43:03', '1');
+  fyh_creacion   DATETIME NULL,
+  fyh_actualizacion DATETIME NULL,
+  estado        VARCHAR (11),
 
--- --------------------------------------------------------
+  FOREIGN KEY (usuario_id) REFERENCES usuarios (id_usuario) on delete no action on update cascade
 
--- Estructura de tabla para la tabla ppffs
+)ENGINE=InnoDB;
+INSERT INTO personas (usuario_id,nombres,apellidos,ci,fecha_nacimiento,profesion,direccion,celular,fyh_creacion,estado)
+VALUES ('1','Freddy Eddy','Hilari Michua','12345678','10/10/1990','LICENCIADO EN EDUCACIÓN','Zona Los Pinos Av. Las Rosas Nro 100','75657007','2023-12-28 20:29:10','1');
+
+CREATE TABLE administrativos (
+
+  id_administrativo      INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  persona_id             INT (11) NOT NULL,
+
+  fyh_creacion   DATETIME NULL,
+  fyh_actualizacion DATETIME NULL,
+  estado        VARCHAR (11),
+
+  FOREIGN KEY (persona_id) REFERENCES personas (id_persona) on delete no action on update cascade
+
+)ENGINE=InnoDB;
+
+
+
+CREATE TABLE docentes (
+
+  id_docente             INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  persona_id             INT (11) NOT NULL,
+  especialidad                VARCHAR (255) NOT NULL,
+  antiguedad                VARCHAR (255) NOT NULL,
+
+
+  fyh_creacion   DATETIME NULL,
+  fyh_actualizacion DATETIME NULL,
+  estado        VARCHAR (11),
+
+  FOREIGN KEY (persona_id) REFERENCES personas (id_persona) on delete no action on update cascade
+
+)ENGINE=InnoDB;
+
+CREATE TABLE estudiantes (
+
+  id_estudiante             INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  persona_id                INT (11) NOT NULL,
+  nivel_id                 INT (11) NOT NULL,
+  grado_id                  INT (11) NOT NULL,
+  rude                   VARCHAR (50) NOT NULL,
+
+  fyh_creacion   DATETIME NULL,
+  fyh_actualizacion DATETIME NULL,
+  estado        VARCHAR (11),
+
+  FOREIGN KEY (persona_id) REFERENCES personas (id_persona) on delete no action on update cascade,
+  FOREIGN KEY (nivel_id) REFERENCES niveles (id_nivel) on delete no action on update cascade,
+  FOREIGN KEY (grado_id) REFERENCES grados (id_grado) on delete no action on update cascade
+
+)ENGINE=InnoDB;
 
 CREATE TABLE ppffs (
-  id_ppff SERIAL PRIMARY KEY,
-  estudiante_id INT NOT NULL,
-  nombres_apellidos_ppff VARCHAR(50) NOT NULL,
-  ci_ppf VARCHAR(20) NOT NULL,
-  celular_ppff VARCHAR(20) NOT NULL,
-  ocupacion_ppff VARCHAR(50) NOT NULL,
-  ref_nombre VARCHAR(50) NOT NULL,
-  ref_parentezco VARCHAR(50) NOT NULL,
-  ref_celular VARCHAR(50) NOT NULL,
-  fyh_creacion TIMESTAMP DEFAULT NULL,
-  fyh_actualizacion TIMESTAMP DEFAULT NULL,
-  estado VARCHAR(11) DEFAULT NULL
-);
 
---
+  id_ppff                                 INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  estudiante_id                           INT (11) NOT NULL,
+  nombres_apellidos_ppff                  VARCHAR (50) NOT NULL,
+  ci_ppf                                  VARCHAR (20) NOT NULL,
+  celular_ppff                            VARCHAR (20) NOT NULL,
+  ocupacion_ppff                          VARCHAR (50) NOT NULL,
+  ref_nombre                              VARCHAR (50) NOT NULL,
+  ref_parentezco                          VARCHAR (50) NOT NULL,
+  ref_celular                             VARCHAR (50) NOT NULL,
+
+
+
+  fyh_creacion   DATETIME NULL,
+  fyh_actualizacion DATETIME NULL,
+  estado        VARCHAR (11),
+
+  FOREIGN KEY (estudiante_id) REFERENCES estudiantes (id_estudiante) on delete no action on update cascade
+
+)ENGINE=InnoDB;
+
+
+
+CREATE TABLE configuracion_instituciones (
+
+  id_config_institucion    INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nombre_institucion       VARCHAR (255) NOT NULL,
+  logo                     VARCHAR (255) NULL,
+  direccion                VARCHAR (255) NOT NULL,
+  telefono                 VARCHAR (100) NULL,
+  celular                  VARCHAR (100) NULL,
+  correo                   VARCHAR (100) NULL,
+
+  fyh_creacion   DATETIME NULL,
+  fyh_actualizacion DATETIME NULL,
+  estado        VARCHAR (11)
+
+)ENGINE=InnoDB;
+INSERT INTO configuracion_instituciones (nombre_institucion,logo,direccion,telefono,celular,correo,fyh_creacion,estado)
+VALUES ('Hilari Web School','logo.jpg','Zona Los Olivos Calle Max Toledo Av. 6 nro 100','2228837','59175657007','info@hilariweb.com','2023-12-28 20:29:10','1');
+
+
+CREATE TABLE gestiones (
+
+  id_gestion      INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  gestion         VARCHAR (255) NOT NULL,
+
+  fyh_creacion   DATETIME NULL,
+  fyh_actualizacion DATETIME NULL,
+  estado        VARCHAR (11)
+
+)ENGINE=InnoDB;
+INSERT INTO gestiones (gestion,fyh_creacion,estado)
+VALUES ('GESTIÓN 2024','2023-12-28 20:29:10','1');
+
+CREATE TABLE niveles (
+
+  id_nivel       INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  gestion_id     INT (11) NOT NULL,
+  nivel          VARCHAR (255) NOT NULL,
+  turno          VARCHAR (255) NOT NULL,
+
+  fyh_creacion   DATETIME NULL,
+  fyh_actualizacion DATETIME NULL,
+  estado        VARCHAR (11),
+
+  FOREIGN KEY (gestion_id) REFERENCES gestiones (id_gestion) on delete no action on update cascade
+
+)ENGINE=InnoDB;
+INSERT INTO niveles (gestion_id,nivel,turno,fyh_creacion,estado)
+VALUES ('1','INICIAL','MAÑANA','2023-12-28 20:29:10','1');
+
+
+CREATE TABLE grados (
+
+  id_grado       INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nivel_id       INT (11) NOT NULL,
+  curso          VARCHAR (255) NOT NULL,
+  paralelo       VARCHAR (255) NOT NULL,
+
+  fyh_creacion   DATETIME NULL,
+  fyh_actualizacion DATETIME NULL,
+  estado        VARCHAR (11),
+
+  FOREIGN KEY (nivel_id) REFERENCES niveles (id_nivel) on delete no action on update cascade
+
+)ENGINE=InnoDB;
+INSERT INTO grados (nivel_id,curso,paralelo,fyh_creacion,estado)
+VALUES ('1','INICIAL - 1','A','2023-12-28 20:29:10','1');
+
+
+CREATE TABLE materias (
+
+  id_materia      INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nombre_materia         VARCHAR (255) NOT NULL,
+
+  fyh_creacion   DATETIME NULL,
+  fyh_actualizacion DATETIME NULL,
+  estado        VARCHAR (11)
+
+)ENGINE=InnoDB;
+INSERT INTO materias (nombre_materia,fyh_creacion,estado)
+VALUES ('MATEMÁTICA','2023-12-28 20:29:10','1');
+
+
+CREATE TABLE asignaciones (
+
+  id_asignacion      INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  docente_id       INT (11) NOT NULL,
+  nivel_id       INT (11) NOT NULL,
+  grado_id       INT (11) NOT NULL,
+  materia_id       INT (11) NOT NULL,
+
+  fyh_creacion   DATETIME NULL,
+  fyh_actualizacion DATETIME NULL,
+  estado        VARCHAR (11),
+
+    FOREIGN KEY (docente_id) REFERENCES docentes (id_docente) on delete no action on update cascade,
+    FOREIGN KEY (nivel_id) REFERENCES niveles (id_nivel) on delete no action on update cascade,
+    FOREIGN KEY (materia_id) REFERENCES materias (id_materia) on delete no action on update cascade,
+        FOREIGN KEY (grado_id) REFERENCES grados (id_grado) on delete no action on update cascade
+
+)ENGINE=InnoDB;
